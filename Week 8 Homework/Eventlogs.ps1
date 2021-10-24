86bd5c32b42b9fe174fab923814587dfcb5050f2
TrippCC <mason.tripp@mymail.champlain.edu>
2021-10-24 13:01:28 -0700

    Completed Week 8 assignment
    The Challenge response is at the end of the EventLogs script

---
+++ Week 8 Homework/Eventlogs.ps1
@@ -0,0 +1,25 @@
+﻿#Storyline: Review the Security Event log
+
+#Directory to save files:
+
+$myDir = "C:\Users\champuser"
+
+#List all the avavilable Windows Event Logs
+Get-Eventlog -list
+
+# Create a prompt to allow user to select the log to view
+$readLog = Read-host -Prompt "Please select a log to review from the list above"
+
+# Print the results for the log
+Get-EventLog -LogName $readLog -Newest 40 | where {$_.Message -ilike "*new process has been*" }
+#| export-csv -NoTypeInformation -Path $myDir\SecurityLogs.csv
+
+# CHALLENGE:
+# Task: Create prompt that allows the user to specify a keyword of phrase to search on
+# Find a string from your even logs to search on 
+
+# Prompt to ask for search string
+$searchString = Read-host -Prompt "Please select search string to filter the logs"
+
+# Print the results of the logs using the string as a filter
+Get-EventLog -LogName $readLog -Newest 40 | where {$_.Message -ilike "*$searchString*" }
---
+++ Week 8 Homework/SendEmailScript.ps1
@@ -0,0 +1,17 @@
+﻿# Storyline: Send an email.
+
+# Body of the email
+# Variable can have an underscore or any alphanumerica value
+$msg = "Hello there."
+
+# echoing to the screen.
+write-host -BackgroundColor Red -ForegroundColor white $msg
+
+#Email from addres
+$email = "mason.tripp@mymail.champlain.edu"
+
+# to address
+$toEmail = "deployer@csi-web"
+
+# Send the email 
+Send-MailMessage -From $email -To $toEmail -Subject "A Greeting" -body $msg -SmtpServer 192.168.6.71
