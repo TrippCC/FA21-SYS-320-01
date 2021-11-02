ac4c3a6add81d0bd4a0ba3434cebe8ee77d46145
TrippCC <mason.tripp@mymail.champlain.edu>
2021-11-02 16:34:05 -0700

    Completed Week 9 Assignment

---
+++ Homework Week 9/CalcStartStop.ps1
@@ -0,0 +1,10 @@
+﻿# Storyline: Use commands to start and stop the calculator application
+
+#help Start-Process -Examples
+Start-Process -FilePath "win32calc"
+
+#help Stop-Process -Examples
+timeout /t 2
+
+Stop-Process -Name "win32calc"
+
---
+++ Homework Week 9/GetProcessService.ps1
@@ -0,0 +1,6 @@
+﻿# Storyline: Using the Get-Process and Get-service
+#Get-Process | Select-Object ProcessName, Path, ID | Export-Csv -Path "C:\Users\champuser\Desktop\myProcesses.csv" -NoTypeInformation
+#Get-Process | Get-Memember
+Get-Service | where { $_.Status -eq "Stopped" } | Export-Csv -Path "C:\Users\champuser\Desktop\myRunningServices.xlsx" -NoTypeInformation
+#Get-Process | where { $_.Status -eq "Stopped" }
+# Export-Csv -Path "C:\Users\champuser\Desktop\myRunningProcesses.csv" -NoTypeInformation
